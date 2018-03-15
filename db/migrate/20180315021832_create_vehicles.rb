56d38f2b027cf4f0e6f498534155e5a405f119f8
miaborjaac <miaborjaac@unal.edu.co>
2018-03-14 21:20:27 -0500

    Vehicle table creation

---
+++ db/migrate/20180315021832_create_vehicles.rb
@@ -0,0 +1,16 @@
+class CreateVehicles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :vehicles do |t|
+      t.string :plate
+      t.integer :user_id
+      t.string :class
+      t.integer :model
+      t.string :color
+      t.integer :capacity
+      t.string :image
+      t.string :brand
+
+      t.timestamps
+    end
+  end
+end
